83887bba189b701cc25a2f376fa24519e39f22c3
Chad Hinds <chadrickj8@gmail.com>
2019-06-02 19:17:31 -0400

    Add encryptedFields to schema

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 const secret = "THis is our little secret!";
-userSchema.plugin(encrypt, {secret: secret})
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['age'] });
 
 const User = new mongoose.model("User", userSchema);
 
