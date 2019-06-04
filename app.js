90d0ff02514b372ef5796b80c6b4cc759889f45f
Chad Hinds <chadrickj8@gmail.com>
2019-06-04 09:10:46 -0400

    Use environment variables

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const app = express();
 const encrypt = require("mongoose-encryption");
+
+const app = express();
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -18,8 +20,7 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-const secret = "This is our little secret!";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
