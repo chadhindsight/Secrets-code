94f4ba371f891bd1d675b92698389198f4ae77c2
Chad Hinds <chadrickj8@gmail.com>
2019-06-13 10:20:46 -0400

    express-session

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+require('express-session')
+
 const app = express();
 
 
